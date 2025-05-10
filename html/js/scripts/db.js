2711fd15bdfe7e56f86dd9b20d9f374b115a32b0
nemethildiko <ildiko.nemeth001@gmail.com>
2025-05-10 10:00:47 +0200

    fatch

--- html/js/scripts/db.js
+++ html/js/scripts/db.js
@@ -1,7 +1,13 @@
-const apiUrl = 'https://nettuts.hu/jms/nemethildiko/';
+const serverUrl = 'https://nettuts.hu/jms/cherryapp/';
 
+export const getAll = (entity = 'products') => {
+    return fetch(`${serverUrl}${entity}`)
+        .then( response => response.json() );
+};
 
-export const getAll = (entity='users')=>{
-    return fetch(apiUrl+entity).then(res=>res.json());
-
+export const remove = (id = 0, entity = 'products') => {
+    return fetch(`${serverUrl}${entity}/${id}`, {
+        method: 'DELETE',
+    })
+    .then( response => response.json() );
 };
